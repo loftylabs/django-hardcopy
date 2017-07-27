eefc8a0dc24209f00477585a7a49210cf932869e
Casey Kinsey <casey.kinsey@gmail.com>
2017-07-27 08:25:00 -0500

    Bump version for hotfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-hardcopy',
-    version='0.1.1',
+    version='0.1.2',
     description='Django wrapper and utils for generating PDFs with headless chrome.',
     author='Casey Kinsey / Lofty Labs',
     author_email='casey@hirelofty.com',
